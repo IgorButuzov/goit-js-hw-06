8f7d2292bab6596febed881826a787fe5afc5501
Igor Butuzov <ibutuzov87@gmail.com>
2022-11-20 20:17:01 +0200

    виконано task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,18 @@
+// Напиши скрипт для створення галереї зображень на 
+// підставі масиву даних.HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення 
+// елементів < img >, вкладених в < li >.Для створення 
+// розмітки використовуй шаблонні рядки і 
+// метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну 
+// операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або 
+// грідами через CSS класи.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +27,40 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+  
+
+  
+  
+for (const image of images) {
+  const imgList = document.createElement("li");
+
+  const imgs = document.createElement("img");
+  imgs.src =  image.url;
+  imgs.alt = image.alt;
+  imgs.classList.add("img");
+  imgList.append(imgs);
+  
+  const pictures = document.querySelector(".gallery");
+  pictures.append(imgList); 
+    console.log(pictures)
+}
+
+
+  
+
+
+// const imgList = document.querySelector(".gallery");
+// document.createElement("li");
+// // console.log(imgList);
+  
+// const imgs = document.createElement("img");
+// imgs.src = images.map(image => image.url);
+// imgs.alt = images.map(image => image.alt);
+// // console.log(imgs)
+
+// for (const image of images) {
+//   console.log(image)
+// }
+
+
